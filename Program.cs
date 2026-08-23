344709d91869f4f47a918a296f6d288adee96bcb
JMCaldas01 <jmcaldas0101@gmail.com>
2026-08-23 11:04:05 -0500

    primer commit de .NET

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// este es un comentario
+
+using System.Reflection;
+
+var assembly = Assembly.GetExecutingAssembly();
+var version = assembly.GetName().Version;
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("This is a sample C# program.");
+Console.WriteLine($"Current Date and Time: {DateTime.Now}");
+Console.WriteLine($"Plataform: {Environment.OSVersion}");
+Console.WriteLine($".NET Version: {Environment.Version}");
+Console.WriteLine($"Assembly Version: {version}");
